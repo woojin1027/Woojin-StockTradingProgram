5500c12a6a490a779ba216730fa257f6a09a76da
woojin <woojinny@kakao.com>
2021-06-28 13:37:25 +0900

    Using KOA studio

---
+++ 32bit.py
@@ -0,0 +1,6 @@
+# For 32 bit it will return 32 and for 64 bit it will return 64
+import struct
+print(struct.calcsize("P") * 8)
+
+import platform
+print(platform.architecture())
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Woojin-StockTradingProgram
+
+### 주식 매매 프로그램 개발 일지
+
+1. 키움 증권을 이용한 프로그램
+2. python 32 bit를 바탕으로한 개발 프로그래밍
